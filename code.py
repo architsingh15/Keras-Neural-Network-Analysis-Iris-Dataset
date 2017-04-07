9829021e3d69199f24613b00da024a289cc86fcd
Archit Singh <archit15singh@gmail.com>
2017-04-08 00:27:26 +0530

    Add files via upload
    First attempt

---
+++ code.py
@@ -0,0 +1,37 @@
+import numpy
+import pandas
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+
+
+seed = 10
+numpy.random.seed(seed)
+
+dataframe = pandas.read_csv("iris.csv", header=None)
+dataset = dataframe.values
+X = dataset[:,0:4].astype(float)
+Y = dataset[:,4]
+
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+dummy_y = np_utils.to_categorical(encoded_Y)
+
+def baseline_model():
+
+	model = Sequential()
+	model.add(Dense(4, input_dim=4, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(3, kernel_initializer='normal', activation='sigmoid'))
+
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=5, verbose=0)
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
